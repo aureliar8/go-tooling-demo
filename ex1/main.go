bdee302ca39fcaf71be4241c38a7e0b07fb1e52b
Aurélien Deroide <aurelien.deroide@exoscale.ch>
2023-03-03 11:03:17 +0100

    Time cli program

--- ex1/main.go
+++ ex1/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	part1()
 	log.Println("Part 1 done")
 	part2()
 	log.Println("Part 2 done")
 	part3()
 	log.Println("Part 3 done")
-	log.Println("Finished sucessfully")
+	log.Println("Finished sucessfully in", time.Now().Sub(start))
 }
